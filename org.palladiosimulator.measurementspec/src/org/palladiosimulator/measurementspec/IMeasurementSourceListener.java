df9e0fdb58e804d436b8ff1caddba37ab7f59411
lehrig <sebastian.lehrig@informatik.tu-chemnitz.de>
2014-04-14 07:45:39 +0000

    Removed ICalculatorListener. Now, IMeasurementSourceListener provides the preUnregister method, thus, making the former obsolete.
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code/MeasurementSpecification/trunk@21805 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

--- org.palladiosimulator.measurementspec/src/org/palladiosimulator/measurementspec/IMeasurementSourceListener.java
+++ org.palladiosimulator.measurementspec/src/org/palladiosimulator/measurementspec/IMeasurementSourceListener.java
@@ -5,4 +5,9 @@ public interface IMeasurementSourceListener {
 
     public void newMeasurementAvailable(Measurement measurement);
 
+    /**
+     * After having registered at a {@link Calculator}, this method gets invoked
+     * to inform the listener about being unregistered.
+     */
+    public void preUnregister();
 }
